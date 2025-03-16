8f80ba5b2e6839ade89aba543fb86a0cc3079efb
nathanmeira <nathan.meira1@gmail.com>
2025-03-15 19:39:21 -0600

    project init + tailwind + pinia

---
+++ front-end/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-6xl font-semibold text-zinc-800">The Designer Archetype</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
