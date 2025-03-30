4b85860e8ac21ec37f64420a18ddc46d58d37a80
nathanmeira <nathan.meira1@gmail.com>
2025-03-29 18:27:39 -0600

    feat: add meta tags

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -21,17 +21,12 @@ export default {
         { property: 'og:description', content: 'Discover your design personality with our quick quiz!' },
         { property: 'og:image', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
         { property: 'og:image:secure_url', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
-        { property: 'og:image:type', content: 'image/jpeg' },  // Specify image type
-        { property: 'og:image:width', content: '1200' },         // Set image width
-        { property: 'og:image:height', content: '630' },         // Set image height
-        { property: 'og:image:alt', content: 'Discover your designer archetype' }, // Alt text for accessibility
+        { property: 'og:image:type', content: 'image/jpeg' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:alt', content: 'Discover your designer archetype' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:url', content: window.location.href }, // Ensure URL is dynamically set
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:image', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
-        { name: 'twitter:image:alt', content: 'Discover your designer archetype' }, // Alt text for Twitter
-        { name: 'twitter:title', content: 'Take the Designer Archetype Quiz' },
-        { name: 'twitter:description', content: 'Discover your design personality with our quick quiz!' },
+        { property: 'og:url', content: window.location.href },
       ]
     };
   }
