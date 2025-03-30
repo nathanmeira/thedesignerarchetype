65aa7e175394e611f82691aa491d0ee4332ce25b
nathanmeira <nathan.meira1@gmail.com>
2025-03-30 12:16:25 -0600

    fix: meta tags weren't working properly

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -11,29 +11,3 @@ import { Analytics } from '@vercel/analytics/nuxt';
   </div>
   <Analytics />
 </template>
-
-<script lang="ts">
-export default {
-  head() {
-    return {
-      meta: [
-        { property: 'og:title', content: 'Take the Designer Archetype Quiz' },
-        { property: 'og:description', content: 'Discover your design personality with our quick quiz!' },
-        { property: 'og:image', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
-        { property: 'og:image:secure_url', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
-        { property: 'og:image:type', content: 'image/jpeg' },
-        { property: 'og:image:width', content: '1200' },
-        { property: 'og:image:height', content: '630' },
-        { property: 'og:image:alt', content: 'Discover your designer archetype' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:url', content: window.location.href },
-      ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        { rel: 'apple-touch-icon', href: '/favicon.png' },
-      ]
-    };
-  }
-
-}
-</script>
--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -17,5 +17,32 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app: {
+    head: {
+      title: 'The Designer Archetype',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Find out your design archetype through our quiz.' },
+        { property: 'og:title', content: 'Take the Designer Archetype Quiz' },
+        { property: 'og:description', content: 'Discover your design personality with our quick quiz!' },
+        { property: 'og:image', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
+        { property: 'og:image:secure_url', content: 'https://rfiaajufzglggcggicou.supabase.co/storage/v1/object/sign/nnathan/perfectionist.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJubmF0aGFuL3BlcmZlY3Rpb25pc3QuanBnIiwiaWF0IjoxNzQzMjY4MTA0LCJleHAiOjE5MDA5NDgxMDR9.Q6-TYJfTvzuImKmY04BStliFFsKGaVZVBlJs7EGHC2A' },
+        { property: 'og:image:type', content: 'image/jpeg' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:alt', content: 'Discover your designer archetype' },
+        { property: 'og:url', content: 'https://thedesignerarchetype.vercel.app/' },
+        { property: 'og:type', content: 'website' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'apple-touch-icon', href: '/favicon.png' }
+      ]
+    }
+  }
 })
